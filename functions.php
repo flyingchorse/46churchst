3b006e1ae9e785b3f15200d9e50a4f6dd7a09d71
David Griffin <dave@digidol-media.net>
2019-09-24 11:39:58 +0100

    Removed deque

--- functions.php
+++ functions.php
@@ -1,8 +1,5 @@
 <?php 
-	function childtheme_enqueue_styles() {
-		
-	wp_dequeue_style( 'parent-style', 
-    get_template_directory_uri() . '/style.css' );		
+	function childtheme_enqueue_styles() {	
 		
   wp_enqueue_style( 'parent-style', 
     get_template_directory_uri() . '/style.css' );
