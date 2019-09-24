56cccf5a7ad2eba978cc5d0eb28a81a045d6d182
David Griffin <dave@digidol-media.net>
2019-09-24 11:37:11 +0100

    Seeing if changing the enque script helps

--- functions.php
+++ functions.php
@@ -1,11 +1,14 @@
 <?php 
 	function childtheme_enqueue_styles() {
+wp_dequeue_style( 'parent-style', 
+    get_template_directory_uri() . '/style.css' )		
+		
   wp_enqueue_style( 'parent-style', 
     get_template_directory_uri() . '/style.css' );
 
   wp_enqueue_style( 'child-style',
     get_stylesheet_directory_uri() . '/style.css',
-    array( 'parent-style' ),
+    array( 'child-style' ),
     wp_get_theme()->get('Version')
   );
   if( is_front_page() ){
